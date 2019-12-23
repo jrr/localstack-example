04c814b95f861c8f0e95571b9b7a82b3240a4741
John Ruble <john.ruble@atomicobject.com>
2019-12-23 09:16:01 -0500

    fix version, add wait

---
+++ wait-for-localstack.sh
@@ -0,0 +1,35 @@
+while $(docker ps -q); do
+  echo 'waiting for container to start..'
+  sleep 1
+done
+
+echo "waiting for S3.."
+until $(nc -zv localstack 4572); do
+    printf '.'
+    sleep 1
+done
+echo "waiting for IAM.."
+until $(nc -zv localstack 4593); do
+    printf '.'
+    sleep 1
+done
+echo "waiting for Lambda.."
+until $(nc -zv localstack 4574); do
+    printf '.'
+    sleep 1
+done
+
+# CONTAINER=$(docker ps -q)
+# echo "waiting for Localstack to report 'Ready.' from container $CONTAINER"
+
+# while true; do
+#   LOGS=$(docker logs $CONTAINER --since 1m)
+#   if echo $LOGS | grep 'Ready.'; then
+#     echo "Localstack is ready!"
+#     break;
+#   fi
+#   echo "waiting.."
+#   sleep 1
+# done
+
+sleep 2
