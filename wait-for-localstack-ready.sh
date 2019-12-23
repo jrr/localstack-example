fd7e0bd243afadb9b44495ec36627eeeab27b5f3
John Ruble <john.ruble@atomicobject.com>
2019-12-23 09:41:09 -0500

    log

--- wait-for-localstack-ready.sh
+++ wait-for-localstack-ready.sh
@@ -1,4 +1,11 @@
 CONTAINER=$(docker ps -q)
+
+if [ -z $CONTAINER ]; then
+    echo "Couldn't find running Docker container."
+    docker --version
+    exit 1
+fi
+
 echo "waiting for Localstack to report 'Ready.' from container $CONTAINER"
 
 while true; do
